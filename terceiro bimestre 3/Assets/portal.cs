03b2a71ab5385f9965b75fa8c880d3efe9b973a3
Script605 <s.raphael@escolar.ifrn.edu.br>
2025-11-26 17:13:35 -0300

    k.4

--- terceiro bimestre 3/Assets/portal.cs
+++ terceiro bimestre 3/Assets/portal.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Portal : MonoBehaviour
+public class PortalCarregarCena : MonoBehaviour
 {
+    [Header("Nome da cena para carregar")]
     public string nomeDaCena;
-    
-    private void OnCollisionEnter2D(Collision2D other)
+
+    private void OnTriggerEnter2D(Collider2D colisao)
     {
-        if (other.gameObject.CompareTag("Player") && nomeDaCena != "")
+        if (colisao.CompareTag("Player"))
         {
-            SceneManager.LoadScene(nomeDaCena);          
+            CarregarCena();
+        }
+    }
+
+    void CarregarCena()
+    {
+        if (!string.IsNullOrEmpty(nomeDaCena))
+        {
+            SceneManager.LoadScene(nomeDaCena);
+        }
+        else
+        {
+            Debug.LogError("Nenhum nome de cena foi definido no portal!");
         }
     }
 }
